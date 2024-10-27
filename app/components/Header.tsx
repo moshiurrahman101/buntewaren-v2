fc4b84337108581d735b6148e8c9813aa5046522
moshiurrahman101 <99rmhridoy@gmail.com>
2024-10-27 17:11:10 +0600

    Menu & Hero

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,35 +1,71 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 import logo from "@/app/assets/image/logo.png";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
+import { AlignRight, X } from "lucide-react";
 
 function Header() {
+  const [menuOn, setMenuOn] = useState(false);
+
+  const mobileMenu = () => {
+    if (menuOn === false) {
+      setMenuOn(true);
+    } else {
+      setMenuOn(false);
+    }
+  };
+
   return (
-    <div className="header flex justify-between items-center mx-5 py-5">
-      <div className="w-[200px]">
-        <Link href={'/'}>
-        <Image src={logo} alt="Buntewaren logo" />
-        </Link>
+    <>
+      <div className="header flex justify-between items-center mx-5 py-5">
+        <div className="w-[200px]">
+          <Link href={"/"}>
+            <Image src={logo} alt="Buntewaren logo" />
+          </Link>
+        </div>
+        <div className="menu lg:block hidden">
+          <ul className="flex gap-6 items-center text-[18px]">
+            <li className="hover:text-green-600">
+              <Link href={"#"}>All Products</Link>
+            </li>
+            <li className="hover:text-green-600">
+              <Link href={"#"}>Nakshi Kantha</Link>
+            </li>
+            <li className="hover:text-green-600">
+              <Link href={"#"}>Ladies Fashion Bag</Link>
+            </li>
+            <li>
+              <Button>Contact</Button>
+            </li>
+          </ul>
+        </div>
+        <div onClick={mobileMenu} className="block lg:hidden">
+          {menuOn ? <X /> : <AlignRight />}
+        </div>
       </div>
-      <div className="menu">
-        <ul className="flex gap-6 items-center text-[18px]">
-          <li className="hover:text-green-600">
-            <Link href={'#'}>All Products</Link>
-          </li>
-          <li className="hover:text-green-600">
-            <Link href={'#'}>Nakshi Kantha</Link>
-          </li>
-          <li className="hover:text-green-600">
-            <Link href={'#'}>Ladies Fashion Bag</Link>
-          </li>
-          <li>
-            <Button>Contact</Button>
-          </li>
-        </ul>
-      </div>
-    </div>
+      {menuOn ? (
+        <div className={`w-full h-screen bg-black text-white absolute`}>
+          <ul className="flex flex-col gap-6 items-center text-[18px] pt-7">
+            <li className="hover:text-green-600">
+              <Link href={"#"}>All Products</Link>
+            </li>
+            <li className="hover:text-green-600">
+              <Link href={"#"}>Nakshi Kantha</Link>
+            </li>
+            <li className="hover:text-green-600">
+              <Link href={"#"}>Ladies Fashion Bag</Link>
+            </li>
+            <li>
+              <Button>Contact</Button>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 
---
+++ app/components/Hero.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+function Hero() {
+  return (
+    <div className="h-[70vh] flex justify-center items-center" id="hero">
+      <div className="w-[700px] text-center">
+        <h1 className="lg:text-3xl md:text-2xl text-2xl font-bold">
+          High-Quality <span className="text-green-600">Jute Bags</span> in
+          Europe
+        </h1>
+        <p className="text-[20px] mt-5">
+          Discover Buntewaren&apos;s stylish, sustainable jute bags imported
+          directly from Bangladesh. Perfect for the eco-conscious European
+          shopper. Shop now and support a greener future.
+        </p>
+        <Button className="mt-5 bg-black">Checkout all product</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
